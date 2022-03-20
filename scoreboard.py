64ad71babccf6b0ee2384b53e45776078febfec2
Jeremy Lynn <Jlynn_209@aol.com>
2022-03-20 09:00:36 -0700

    Added collison and scoreboard

---
+++ scoreboard.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.penup()
+        self.hideturtle()
+        self.goto(self.xcor(), 260)
+        self.color("white")
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
+
+    def increase_score(self):
+        self.clear()
+        self.score += 1
+        self.update_scoreboard()
